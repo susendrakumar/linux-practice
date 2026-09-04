b420dc37d65566fc348499c51152220706391979
susendrakumar <82529889+susendrakumar@users.noreply.github.com>
2026-09-04 13:52:38 +0530

    Update hello.sh

--- Jenkins-Task-2/hello.sh
+++ Jenkins-Task-2/hello.sh
@@ -5,3 +5,4 @@ echo "GitHub commit successfully triggered Jenkins build."
 date
 echo "Automatic webhook build test successful!"
 echo "Webhook trigger test 2"
+echo "Final automatic Jenkins email test"
